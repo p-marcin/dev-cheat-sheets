5454e07bc1a32975c8ac2ec8de7d7bf43c86bdd6
Marcin P. <114195537+p-marcin@users.noreply.github.com>
2025-02-16 11:12:32 +0000

    Add Algolia Search

--- scripts/generate-local.sh
+++ scripts/generate-local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # GENERATES LOCAL SITE
 
+export SITE_SEARCH_PROVIDER=docsearch
 npx antora generate --fetch --clean "${@}" "../local-playbook.yml"
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # GENERATES SITE (USED BY GITHUB WORKFLOW)
 
+export SITE_SEARCH_PROVIDER=docsearch
 npx antora generate --fetch --clean "${@}" "antora-playbook.yml"
